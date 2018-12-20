40895232cf1da56cdf0eaa20ee79538f13ca4c92
Tomas Blake <lonsky.to@gmail.com>
2018-12-20 20:57:37 +0100

    Initial Commit

---
+++ DatingApp.Api/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using DatingApp.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext (DbContextOptions<DataContext> options) : base (options) { }
+        public DbSet<Value> Values { get; set; }
+        //protected override void OnConfiguring (DbContextOptionsBuilder optionBuilder)
+        //{
+        //    optionBuilder.UseSqlServer(@"Server=PC-HB;Database=CoreDb;Trusted_Connection=True;");
+        //}
+    }
+}
