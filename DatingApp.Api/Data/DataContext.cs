becd644c5ce15c07bf5afd4d5ecf9334d33fa0e1
Tomas Blake <lonsky.to@gmail.com>
2018-12-31 18:29:47 +0100

    End of section 8

--- DatingApp.Api/Data/DataContext.cs
+++ DatingApp.Api/Data/DataContext.cs
@@ -6,9 +6,9 @@ namespace DatingApp.Api.Data
     public class DataContext : DbContext
     {
         public DataContext (DbContextOptions<DataContext> options) : base (options) { }
-        
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users {get; set;}
+        public DbSet<Photo> Photos {get; set;}
         //protected override void OnConfiguring (DbContextOptionsBuilder optionBuilder)
         //{
         //    optionBuilder.UseSqlServer(@"Server=PC-HB;Database=CoreDb;Trusted_Connection=True;");
--- DatingApp.Api/Startup.cs
+++ DatingApp.Api/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.Api.Data;
 using DatingApp.Api.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -34,9 +35,13 @@ namespace DatingApp.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().AddJsonOptions(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+            .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))); //Zde se vloží expression s nastavením Db serveru
             services.AddCors();
+            services.AddAutoMapper();
+            services.AddScoped<IDatingRepository, DatingRepository>();
+            services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(options => {
@@ -52,7 +57,7 @@ namespace DatingApp.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
         {
             if (env.IsDevelopment())
             {
@@ -77,6 +82,7 @@ namespace DatingApp.Api
             }
 
             //app.UseHttpsRedirection();
+            seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());    
             app.UseAuthentication();
             app.UseMvc();
